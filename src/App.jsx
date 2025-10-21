545afc2725c8a1f51367aff22e1bc63ac96d3221
Seth Ganan <seth.ganan@drake.edu>
2025-10-21 15:21:30 -0500

    final checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,9 @@ function App() {
             type="checkbox"
             checked={task.completed}
             onChange={() => handleToggle(index)}/>
-            <span className="task-text">{task.text}</span>
+            <span className={task.completed ? "task-text completed" : "task-text"}>
+            {task.text}
+            </span>            
             <button
               className="delete-button"
               onClick={() => handleDelete(index)}
